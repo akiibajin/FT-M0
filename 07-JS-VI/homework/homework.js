50aa66d6c92c54378fedcd8b3a863c7a53273bbe
hector <julianrobayo8000@gmail.com>
2021-07-07 19:47:33 -0500

    homework jsvi

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,28 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  this.nombre=nombre;
+  nombre2=nombre.charAt(0).toUpperCase();
+  for(var i=1;i<=nombre.length;i++){
+    nombre2=nombre2+nombre.charAt(i);
+    
+  }
+  return nombre2;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  this.n1=n1;
+  this.n2=n2;
+cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +33,24 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-}
+    this.numeros=numeros;
+    resul=0;
+    for(i=0;i<numeros.length;i++){
+      if(numeros[i]%1!==0){
+        continue;
+    }
+      resul=resul+numeros[i];
+    }
+    cb(resul);
+  }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  var array2=array.forEach(function(fn){
+    cb(fn);
+  })
 }
 
 function map(array, cb) {
@@ -35,12 +58,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+var array2=array.map(function(fn){
+  return cb(fn);
+})
+return array2;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+array2=array.filter(function(fn){
+  return fn==="a";
+})
+return array2;
 }
 
 // No modificar nada debajo de esta línea
